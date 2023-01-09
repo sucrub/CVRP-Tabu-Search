1afcb3cca231041d35c79374c7ab40a9dd243360
sucrub <bachspham@gmail.com>
2023-01-09 17:14:56 +0700

    Building

--- generate.cpp
+++ generate.cpp
@@ -7,19 +7,59 @@ int *cap;
 int numberOfCar;
 int capEachCar;
 
-int main()
+bool checkCondition()
 {
-
-    srand(time(NULL));
-    N = rand() % (15 - 6 + 1) + 6;
-    numberOfCar = rand() % (6 - 3 + 1) + 3;
-    capEachCar = rand() % (25 - 15 + 1) + 15;
-    graph = new int *[N];
+    int sumCapCanStorage = numberOfCar * capEachCar;
+    int sumCapCustomer = 0;
     for (int i = 0; i < N; i++)
     {
-        graph[i] = new int[N];
+        sumCapCustomer += cap[i];
     }
-    cap = new int[N];
+    if (sumCapCustomer > sumCapCanStorage)
+        return false;
+    else
+        return true;
+}
+
+int main()
+{
+    srand(time(NULL));
+    do
+    {
+        N = rand() % (15 - 6 + 1) + 6;
+        numberOfCar = rand() % (6 - 3 + 1) + 3;
+        capEachCar = rand() % (40 - 20 + 1) + 20;
+        graph = new int *[N];
+        for (int i = 0; i < N; i++)
+        {
+            graph[i] = new int[N];
+        }
+        cap = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+
+                if (i == j)
+                {
+                    graph[i][j] = 0;
+                    graph[j][i] = 0;
+                }
+                if (i > j)
+                    continue;
+                if (i < j)
+                {
+                    graph[i][j] = rand() % (20 - 1 + 1) + 1;
+                    graph[j][i] = graph[i][j];
+                }
+            }
+        }
+        for (int i = 1; i < N; i++)
+        {
+            cap[i] = rand() % (10 - 3 + 1) + 3;
+        }
+    } while (checkCondition == false);
+
     ofstream f;
     f.open("test.txt");
     f << N << endl;
@@ -29,34 +69,11 @@ int main()
     {
         for (int j = 0; j < N; j++)
         {
-
-            if (i == j)
-            {
-                graph[i][j] = 0;
-                graph[j][i] = 0;
-            }
-            if (i > j)
-                continue;
-            if (i < j)
-            {
-                graph[i][j] = rand() % (20 - 1 + 1) + 1;
-                graph[j][i] = graph[i][j];
-            }
-        }
-    }
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
             f << graph[i][j] << " ";
         }
         f << endl;
     }
     cap[0] = 0;
-    for (int i = 1; i < N; i++)
-    {
-        cap[i] = rand() % (10 - 3 + 1) + 3;
-    }
     for (int i = 0; i < N; i++)
     {
         f << cap[i] << endl;
--- main.cpp
+++ main.cpp
@@ -58,9 +58,70 @@ int fitness(vector<int> *SOL)
     return result;
 }
 
-// Making first solution
-vector<int> *firstSol()
+// Checking if the solution is valid or not
+bool checkCondition(vector<int> *sol)
 {
+    int value[P] = {0};
+    for (int i = 0; i < P; i++)
+    {
+        for (int j = 0; j < sol[i].size(); j++)
+        {
+            value[i] += COST[sol[i][j]];
+        }
+    }
+    for (int i = 0; i < P; i++)
+    {
+        if (value[i] > C)
+            return false;
+    }
+    return true;
+}
+
+// Making first solution
+void firstSol()
+{
+    SOLUTION = new vector<int>[P];
+    for (int i = 0; i < P; i++)
+    {
+        SOLUTION[i].push_back(0);
+    }
+    do
+    {
+        for (int i = 1; i < N; i++)
+        {
+            int route = rand() % (P - 0);
+            SOLUTION[route].push_back(i);
+        }
+        if (checkCondition == false)
+        {
+            for (int i = 0; i < P; i++)
+            {
+                SOLUTION[i].clear();
+            }
+            for (int i = 0; i < P; i++)
+            {
+                SOLUTION[i].push_back(0);
+            }
+        }
+    } while (checkCondition == false);
+}
+
+void printSolution(vector<int> *sol)
+{
+    int cap[P] = {0};
+    for (int i = 0; i < P; i++)
+    {
+        cout << "Route " << i << ": ";
+        for (int j = 0; j < sol[i].size(); j++)
+        {
+            cap[i] += COST[sol[i][j]];
+            cout << sol[i][j] << " ";
+        }
+        cout << endl;
+        cout << "Storage of route " << i << ": ";
+        cout << cap[i] << "/" << C;
+        cout << endl;
+    }
 }
 
 // Main algorithm
@@ -70,7 +131,13 @@ void tabuSearch()
 
 int main()
 {
+    srand(time(NULL));
     readFile();
     SOLUTION = new vector<int>[N];
     tabuSearch();
+    firstSol();
+    cout << "First initialize solution: " << endl;
+    printSolution(SOLUTION);
+    cout << endl
+         << "SOLUTION: " << fitness(SOLUTION); // dang khong chay duoc ham nay
 }
